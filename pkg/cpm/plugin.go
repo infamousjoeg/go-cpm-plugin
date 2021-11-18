9fd7fafcdf26356b968db9fa7c3a1422b0eeab9f
Joe Garcia <joe@joe-garcia.com>
2021-11-17 21:20:45 -0500

    Initial commit

---
+++ pkg/cpm/plugin.go
@@ -0,0 +1,12 @@
+package cpm
+
+import (
+	"github.com/infamousjoeg/go-cpm-plugin/pkg/types"
+)
+
+var Account types.Account
+
+// ParseAccountXML parses account data from a CPM-provided XML file.
+func ParseAccountXML(xml string) (Account, error) {
+	return Account{}, nil
+}
